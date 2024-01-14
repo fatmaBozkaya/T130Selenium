b9c21aed7c4a79ad755641f552993ebed23ba687
bozkaya <fisik455@gmail.com>
2024-01-14 13:19:59 +0300

    first commit

--- src/test/java/T130_SeleniumPractice/M04.java
+++ src/test/java/T130_SeleniumPractice/M04.java
@@ -9,38 +9,33 @@ import java.time.Duration;
 public class M04 {
     public static void main(String[] args) {
 
-//1- https:www.google.com adresine gidilir
-//2- Sayfanın title ve Url değerleri yazdırılır
-//3- Yeni bir sekme açılarak www.wisequarter.com adresine gidilir ve
-//4- Gidilen sayfanın title ve url değerleri yazdırılır ve
-//   title'in wisequarter içerip içermediği kontrol edilir ve bilgi verilir
-//5- Ilk sekmeye geçilip, burada www.testotomasyonu.com adresine gidilir
-//6- Gidilen sayfanın title ve url değerleri yazdırılır ve
-//   title'in testotomasyonu içerip içermediği kontrol edilir ve bilgi verilir
-//7- Açılan pencere kapatılır.
-
         System.setProperty("webdriver.chrome.driver","src/kurulumDosyalari/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //sayfa 10 sn icinde yüklenmezse testi durdurur
         //10 saniye dolmadan yüklenirse 10 saniye beklemez
-
+        //1- https:www.google.com adresine gidilir
         driver.get("https://www.google.com");
+        //2- Sayfanın title ve Url değerleri yazdırılır
         System.out.println("İlk site Title:"+driver.getTitle());
 
         System.out.println("İlk site Url:"+driver.getCurrentUrl());
-String handle1=driver.getWindowHandle();
+        String handle1=driver.getWindowHandle();
         System.out.println("İlk site Window Handle:"+driver.getWindowHandle());
 
         driver.switchTo().newWindow(WindowType.TAB);
+        //3- Yeni bir sekme açılarak www.wisequarter.com adresine gidilir ve
         driver.navigate().to("https://www.wisequarter.com");
+        //4- Gidilen sayfanın title ve url değerleri yazdırılır ve
         String actualTitle=driver.getTitle();
         System.out.println("İkinci site Title:"+driver.getTitle());
 
         System.out.println("İkinci site Url:"+driver.getCurrentUrl());
 
         System.out.println("İkinci site Window Handle:"+driver.getWindowHandle());
+
+        //   title'in wisequarter içerip içermediği kontrol edilir ve bilgi verilir
         String exceptedWord="wisequarter";
 
         if (actualTitle.contains(exceptedWord)){
@@ -48,15 +43,18 @@ String handle1=driver.getWindowHandle();
         }else {
             System.out.println("Title"+exceptedWord+"içermiyor");
         }
-driver.switchTo().window(handle1);
-        driver.navigate().to("https://www.testotomasyonu.com");
+        //5- Ilk sekmeye geçilip, burada www.testotomasyonu.com adresine gidilir
+        driver.switchTo().window(handle1);
 
+        driver.navigate().to("https://www.testotomasyonu.com");
+//6- Gidilen sayfanın title ve url değerleri yazdırılır ve
 String sonTitle=driver.getTitle();
         System.out.println("Son site Title:"+driver.getTitle());
 
         System.out.println("Son site Url:"+driver.getCurrentUrl());
         String handle2=driver.getWindowHandle();
         System.out.println("Son site Window Handle:"+driver.getWindowHandle());
+        //   title'in testotomasyonu içerip içermediği kontrol edilir ve bilgi verilir
         String exceptedWord2="testotomasyonu";
 
         if (sonTitle.contains(exceptedWord2)){
@@ -64,6 +62,7 @@ String sonTitle=driver.getTitle();
     }else{
         System.out.println("Title"  +exceptedWord2+ "içermiyor");
     }
+        //7- Açılan pencere kapatılır.
 driver.quit();
         }
 
--- src/test/java/T130_SeleniumPractice/M05.java
+++ src/test/java/T130_SeleniumPractice/M05.java
@@ -1,23 +1,63 @@
 package T130_SeleniumPractice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class M05 {
-    public static void main(String[] args) {
-        /*
-          1-Driver oluşturalim
-          2-Java class'imiza chnomedriver.exe i tanitalim
-          3-Driver'in tum ekranı kaplamasini saglayalim
-          4-"https://www.otto.de” adresine gidelim
-          5-Driverla sayfanın yuklenmesini 10.000 milisaniye boyunca beklesini
-            söyleyelim. Egen oncesinde sayfa Yüklenirse, beklemeyi bıraksın.
-          6-Bu web adresinin sayfa basligini (title) ve adres (url)ini alalim
-          7-Title ve url inin "OTTO” kelimesinin içerip icermedigini kontrol edelim
-          8-Ardindan "https://wisequarter.com/” adresine gidip
-          9-Bu adresin basligini alalim ve "Quarter” kelimesini icenip icermedigini kontrol edelim
-          10-Bir onceki web sayfamiza geri donelim
-          11-Sayfayı yenileyelim
-          12-Daha sonra web sayfamiza tekrar donelim ve oldugumuuz sayfayi kapatalim
-          13- En son adim olarak butun sayfalarimizi kapatmis olalim
-         */
+    public static void main(String[] args) throws InterruptedException {
+
+         // 1-Driver oluşturalim
+        WebDriver driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+         // 2-Java class'imiza chnomedriver.exe i tanitalim
+        System.setProperty("webdriver.chrome.driver","src/kurulumDosyalari/chromedriver.exe");
+         // 3-Driver'in tum ekranı kaplamasini saglayalim
+        driver.manage().window().maximize();
+         // 4-"https://www.otto.de” adresine gidelim
+        driver.get("https://www.otto.de");
+         // 5-Driverla sayfanın yuklenmesini 10.000 milisaniye boyunca beklesini
+         //   söyleyelim. Egen oncesinde sayfa Yüklenirse, beklemeyi bıraksın.
+        Thread.sleep(1000);
+        //  6-Bu web adresinin sayfa basligini (title) ve adres (url)ini alalim
+        String actualTitle=driver.getTitle();
+        System.out.println("site Title:"+driver.getTitle());
+
+        System.out.println("site Url:"+driver.getCurrentUrl());
+        String handle1=driver.getWindowHandle();
+        System.out.println("site Window Handle:"+driver.getWindowHandle());
+
+        driver.switchTo().newWindow(WindowType.TAB);
+         // 7-Title ve url inin "OTTO” kelimesinin içerip icermedigini kontrol edelim
+        String exceptedWord="OTTO";
+
+        if (actualTitle.contains(exceptedWord)){
+            System.out.println("Title " + exceptedWord + " içeriyor");
+        }else {
+            System.out.println("Title " +exceptedWord+  " içermiyor");
+        }
+         // 8-Ardindan "https://wisequarter.com/” adresine gidip
+        driver.get("https://wisequarter.com/");
+         // 9-Bu adresin basligini alalim ve "Quarter” kelimesini icenip icermedigini kontrol edelim
+        String exceptedWord1="Quarter";
+
+        if (actualTitle.contains(exceptedWord)){
+            System.out.println("Title"+ exceptedWord1 + " içeriyor");
+        }else {
+            System.out.println("Title"+ exceptedWord1 +" içermiyor");
+        }
+         // 10-Bir onceki web sayfamiza geri donelim
+
+        driver.navigate().back();
+        driver.get("https://www.otto.de");
+        //  11-Sayfayı yenileyelim
+        driver.navigate().refresh();
+        //  12-Daha sonra web sayfamiza tekrar donelim ve oldugumuuz sayfayi kapatalim
+        driver.quit();
+        //  13- En son adim olarak butun sayfalarimizi kapatmis olalim
+
 
     }
 }
